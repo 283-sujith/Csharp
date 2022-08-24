38eae30de3267cdd584ce93fb04e97fe8f7c0ba3
283-sujith <105536420+283-sujith@users.noreply.github.com>
2022-08-24 16:05:28 +0530

    Add files via upload

---
+++ ConsoleAppDemo/outparameter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class outparameter
+    {
+        public static void smallno(int a,int b,out int small)
+            {
+            if(a<b)
+            {
+                small = a;
+            }
+            else
+                small=b;
+        }
+        public static void Main()
+        {
+            int a, b, small;
+            Console.WriteLine("enter a");
+            a=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter b");
+            b= int.Parse(Console.ReadLine());
+            smallno(a,b,out small);
+            Console.WriteLine($"THE SMALLEST IS {small}");
+        }
+    }
+}
---
+++ ConsoleAppDemo/sujith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class sujith
+    
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input a first number(<10): ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input a second number(>20): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((m < 10 && n > 20));
+        }
+    }
+
+}
+
+
+
+
+
